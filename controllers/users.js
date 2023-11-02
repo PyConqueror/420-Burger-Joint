36eb595a11db595cceda46b747f25d87ea5a16a2
Wan Aqim <wanaqim@gmail.com>
2023-11-02 16:25:17 +0800

    add users controller function and route

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,28 @@
 const User = require('../model/user');
+const passport = require('passport')
 
 module.exports = {
+    registerPage,
+    loginPage,
+    login,
     register,
+    logout
 };
+
+function registerPage(req, res) {
+    res.render('users/register')
+}
+
+function loginPage(req, res) {
+    res.render('users/login'); 
+}
+
+function login(req, res, next) {
+    passport.authenticate('local', {
+        successRedirect: '/',         
+        failureRedirect: '/users/login' 
+    })(req, res, next);
+}
 
 async function register(req, res) {
     const user = new User(req.body);
@@ -10,6 +30,7 @@ async function register(req, res) {
     res.redirect('/users/login')
     }
 
-
-
-
+function logout(req, res) {
+    req.logout();
+    res.redirect('/');
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/login', usersController.loginPage);
+router.post('/login', usersController.login);
+router.get('/logout', usersController.logout);
+router.get('/register', usersController.registerPage);
+router.post('/register', usersController.register);
 
 module.exports = router;
+
