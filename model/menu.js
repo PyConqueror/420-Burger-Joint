c46596aec37a1e622875848ae6e49898db73ec6e
Wan Aqim <wanaqim@gmail.com>
2023-11-02 11:19:50 +0800

    add database and schema

---
+++ model/menu.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema({
+    name: String,
+    description: String,
+    price: Number,
+    imagePath: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('MenuItem', menuSchema);
---
+++ model/review.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    menuItem: {
+        type: Schema.Types.ObjectId,
+        ref: 'MenuItem',
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
