1943395c22d934d9766a96dde5dd992326713f2f
Wan Aqim <wanaqim@gmail.com>
2023-11-02 16:44:03 +0800

    passport modification

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
