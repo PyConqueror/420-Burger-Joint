ea9f0e2c3c59b9afd4cf59f23bc8815926d67428
Wan Aqim <wanaqim@gmail.com>
2023-11-03 17:18:24 +0800

    add register account page

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,41 @@
 const User = require('../model/user');
 const passport = require('passport')
 
+module.exports = {
+    registerPage,
+    loginPage,
+    login,
+    register,
+    logout
+};
+
+function registerPage(req, res) {
+    res.render('users/register', {msg: ' '})
+}
+
+function loginPage(req, res) {
+    res.render('users/login'); 
+}
+
+function login(req, res, next) {
+    passport.authenticate('local', {
+        successRedirect: '/',         
+        failureRedirect: '/users/login' 
+    })(req, res, next);
+}
+
+async function register(req, res) {
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+        res.render('users/register',{ error: 'Username already taken. Please choose another.' })
+        return
+    }
+    const user = new User(req.body);
+    await user.save();
+    res.redirect('/menus')
+    }
+
+function logout(req, res) {
+    req.logout();
+    res.redirect('/');
+}
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/users');
 
-// router.get('/login', usersController.loginPage);
-// router.post('/login', usersController.login);
-// router.get('/logout', usersController.logout);
-// router.get('/register', usersController.registerPage);
-// router.post('/register', usersController.register);
+router.get('/login', usersController.loginPage);
+router.post('/login', usersController.login);
+router.get('/logout', usersController.logout);
+router.get('/register', usersController.registerPage);
+router.post('/register', usersController.register);
 
 module.exports = router;
 
