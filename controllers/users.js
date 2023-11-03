49936b847f87c3b70a0c11b384036d4e60de0159
Wan Aqim <wanaqim@gmail.com>
2023-11-03 10:05:02 +0800

    test

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,36 @@
 const User = require('../model/user');
 const passport = require('passport')
 
-// module.exports = {
-//     registerPage,
-//     loginPage,
-//     login,
-//     register,
-//     logout
-// };
+module.exports = {
+    registerPage,
+    loginPage,
+    login,
+    register,
+    logout
+};
 
-// function registerPage(req, res) {
-//     res.render('users/register')
-// }
+function registerPage(req, res) {
+    res.render('users/register')
+}
 
-// function loginPage(req, res) {
-//     res.render('users/login'); 
-// }
+function loginPage(req, res) {
+    res.render('users/login'); 
+}
 
-// function login(req, res, next) {
-//     passport.authenticate('local', {
-//         successRedirect: '/',         
-//         failureRedirect: '/users/login' 
-//     })(req, res, next);
-// }
+function login(req, res, next) {
+    passport.authenticate('local', {
+        successRedirect: '/',         
+        failureRedirect: '/users/login' 
+    })(req, res, next);
+}
 
-// async function register(req, res) {
-//     const user = new User(req.body);
-//     await user.save();
-//     res.redirect('/users/login')
-//     }
+async function register(req, res) {
+    const user = new User(req.body);
+    await user.save();
+    res.redirect('/users/login')
+    }
 
-// function logout(req, res) {
-//     req.logout();
-//     res.redirect('/');
-// }
+function logout(req, res) {
+    req.logout();
+    res.redirect('/');
+}
